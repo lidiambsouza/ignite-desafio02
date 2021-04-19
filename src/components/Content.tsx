f8c42c57bb15699edd4b77a68e9a78d9e292fea5
lidiambsouza <lidiambsouza@gmail.com>
2021-04-18 21:50:53 -0300

    finalizado o desafio 02

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,45 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { MovieCard } from './MovieCard';
+import '../styles/content.scss';
+
+
+interface ContentProps {
+  onMovie: {
+    imdbID: string,
+    Title: string,
+    Poster: string,
+    Ratings: Array<{
+      Source: string,
+      Value: string,
+    }>,
+    Runtime: string,
+  }[];
+  title:string;
+}
+
+
+
+export function Content({onMovie,title}:ContentProps) {
+  
+
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {onMovie.map(movie => (
+          <MovieCard 
+          key ={movie.imdbID} 
+          title={movie.Title} 
+          poster={movie.Poster} 
+          runtime={movie.Runtime} 
+          rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
+import { useEffect, useState } from 'react';
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+
+ interface SiderBarProps{
+  onGenres:{
+    id: number,
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family',
+    title: string,
+  }[];
+  onNewGenres: (id:number)=>void;
+  onSelectedGenreId: number;
+ }
+
+export function SideBar({onGenres,onNewGenres,onSelectedGenreId}:SiderBarProps) {
+  
+
+  return (
+    <nav className="sidebar">
+      <span>Lili<p>Movies</p></span>
+
+      <div className="buttons-container">
+        {onGenres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onNewGenres(genre.id)}
+            selected={onSelectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
   // Complete aqui
 }
